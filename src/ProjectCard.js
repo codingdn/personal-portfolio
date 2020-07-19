cc173cd4c74f931dc706358d64371860cc2d4bc0
Codingdn <danthedevnguyen@gmail.com>
2020-07-18 23:57:24 -0400

    Create ProjectCard.js

---
+++ src/ProjectCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+//template for displaying projects
+//follow ig guidelines
+
+function ProjectCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProjectCard
