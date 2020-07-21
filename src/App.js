b8e8b1866c60a60a86b2d0d7d5ee9ace12fd9765
Codingdn <danthedevnguyen@gmail.com>
2020-07-20 21:24:21 -0400

    added some styling

--- src/App.js
+++ src/App.js
@@ -12,21 +12,9 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import EmailIcon from "@material-ui/icons/Email";
 import ResumePDF from "./Files/Resume.pdf";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import {Switch, FormControlLabel} from '@material-ui/core';
-import Brightness4Icon from '@material-ui/icons/Brightness4';
-import Brightness7Icon from '@material-ui/icons/Brightness7';
 /**
  *Working on dark mode right now
  */
-
-// const themeObject = {
-//   pallete: {
-//     primary: {main: '#053f5c'},
-//     secondary: {main: '#5e3c6f'},
-//     type: 'dark'
-//   }
-// }
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -44,29 +32,10 @@ const skillset = [
   { skill: "Git" },
 ];
 
-// const useDarkMode = () => {
-//   const [theme, setTheme] = useState(themeObject);
-//   const {palette: type} = theme;
-//   const toggleDarkMode = () => {
-//     const updatedTheme = {
-//       ...theme,
-//       pallete: {
-//         ...theme.pallete,
-//         type: type ==='light' ? 'dark': 'light'
-//       }
-//     }
-//     setTheme(updatedTheme);
-//   }
-//   return(theme);
-// }
-
 function App() {
   const classes = useStyles();
   const [projects, setProjects] = useState([]);
-  //const [theme, toggleDarkMode] = useDarkMode();
-
-  //const themeConfig = createMuiTheme(theme);
-  //console.log(themeConfig)
+  
 
   useEffect(() => {
     db.collection("Projects").onSnapshot((snapshot) => {
@@ -79,182 +48,187 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <div className="app__header">
-        <AnchorLink offset="100" href="#Intro">
-          <img
-            className="app__logo"
-            height="50px"
-            width="150px"
-            src={PortfolioLogo}
-          />
-        </AnchorLink>
-        <div className="app__headerButtons">
-          <FormControlLabel control={<Switch onClick={()=> console.log('Dark mode toggled')}/>}/>
-          <AnchorLink offset="300" href="#About" className="app__anchor">
-            <Button className={classes.button}>About</Button>
-          </AnchorLink>
-          <AnchorLink offset="100" href="#Projects" className="app__anchor">
-            <Button className={classes.button}>Projects</Button>
-          </AnchorLink>
-          <AnchorLink offset="100" href="#Contact" className="app__anchor">
-            <Button className={classes.button}>Contact</Button>
-          </AnchorLink>
-
-          {/**This button will redirect to pdf of my current resume */}
-          <Button className={classes.button}>
-            <a href={ResumePDF} download className="app__headerButtonsResume">
-              Resume
-            </a>
-          </Button>
-        </div>
-      </div>
-      {/**------------------------------------------------- */}
-      <section id="Intro">
-        <div className="app__introduction">
-          <article>
-            <h1>
-              <strong>Hi there!</strong> 👋
-            </h1>
-            <h1>
-              I'm <strong>Daneil Nguyen</strong>, a current college student who
-              is a tech enthusiast and aspiring full-stack developer 💻
-            </h1>
-          </article>
-        </div>
-      </section>
-      {/**------------------------------------------------- */}
-      <section id="About">
-        <div className="app__about">
-          <div className="app__aboutDescription">
-            <center>
-              <h1>
-                {/* <strong> */}
-                <u>About Me</u>
-                {/* </strong> */}
-              </h1>
-            </center>
-            <p>
-              Hi, I'm Daneil Nguyen, a current college student who is a tech
-              enthusiast and aspiring full-stack developer.
-            </p>
-            <br />
-            <p>
-              I am based in the DMV and am attending the University of Maryland,
-              College Park as an upcoming freshman this fall. I will be studying
-              Computer Science with a focus on Data Science.
-            </p>
-            <br />
-            <p>
-              My overarching goal is to be able to utilize technology to bring
-              forth positive change in this world. However, I love programming
-              and have gained an interest in web development and UI/UX recently.
-              My goal is to learn full-stack development so that I can create
-              and deploy modern web applications.
-            </p>
-            <br />
-
-            <center>
-              <h3>Skill Set/Technologies:</h3>
-            </center>
-            <div className="app__skills">
-              {skillset.map((skill) => (
-                <div className="app__skillsIndividual">
-                  <h4>⚪{skill.skill}</h4>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="app__profilePic">
-            <img
-              src={PortfolioPic}
-              alt="profile pic"
-              height="400px"
-              width="350px"
-            />
-          </div>
-        </div>
-      </section>
-      {/**------------------------------------------------- */}
-
-      {/**------------------------------------------------- */}
-      {/**Possibly use firebase so that new projects can be easily added. Map */}
-      <section id="Projects">
-        <div className="app__projects">
-          <center>
-            <h1>My Projects</h1>
-          </center>
-          <div className="app__projectList">
-            {projects.map(({ project }) => (
-              <ProjectCard
-                projectName={project.projectName}
-                image={project.image}
-                description={project.description}
-                github={project.github}
-                technologies={project.technologies}
+      <div className="App">
+          <div className="app__header">
+            <AnchorLink offset="100" href="#Intro">
+              <img
+                className="app__logo"
+                height="50px"
+                width="150px"
+                src={PortfolioLogo}
               />
-            ))}
-          </div>
-        </div>
-      </section>
-      {/**------------------------------------------------- */}
-      <section id="Contact">
-        <div className="app__contact">
-          <div className="app__contactText">
-            <center>
-              <h1>Get In Touch</h1>
-            </center>
-            <p>
-              My inbox is always open for new opportunities for employment,
-              internships, etc. Otherwise, you can ask me questions and simply
-              say hi!
-            </p>
+            </AnchorLink>
+            <div className="app__headerButtons">
+              
+              <AnchorLink offset="300" href="#About" className="app__anchor">
+                <Button className={classes.button}>About</Button>
+              </AnchorLink>
+              <AnchorLink offset="100" href="#Projects" className="app__anchor">
+                <Button className={classes.button}>Projects</Button>
+              </AnchorLink>
+              <AnchorLink offset="100" href="#Contact" className="app__anchor">
+                <Button className={classes.button}>Contact</Button>
+              </AnchorLink>
 
-            {/**https://stackoverflow.com/questions/50350085/how-to-make-a-hyperlink-external-in-react */}
-            <div className="app__contactButtons">
-              <button>
+              {/**This button will redirect to pdf of my current resume */}
+              <Button className={classes.button}>
                 <a
-                  href="mailto: danthedevnguyen@gmail.com"
-                  className="app__contactIcons"
+                  href={ResumePDF}
+                  download
+                  className="app__headerButtonsResume"
                 >
-                  <EmailIcon />
+                  Resume
                 </a>
-              </button>
-              <button>
-                <a
-                  href="https://www.linkedin.com/in/daneil-nguyen"
-                  className="app__contactIcons"
-                >
-                  <LinkedInIcon />
-                </a>
-              </button>
-              <button>
-                <a
-                  href="https://github.com/codingdn"
-                  className="app__contactIcons"
-                >
-                  <GitHubIcon />
-                </a>
-              </button>
-              <button>
-                <a
-                  href="https://www.instagram.com/coding.dan/"
-                  className="app__contactIcons"
-                >
-                  <InstagramIcon />
-                </a>
-              </button>
+              </Button>
             </div>
           </div>
-        </div>
-      </section>
+          {/**------------------------------------------------- */}
+          <section id="Intro">
+            <div className="app__introduction">
+              <article>
+                <h1>
+                  <strong>Hi there!</strong> 👋
+                </h1>
+                <h1>
+                  I'm <strong>Daneil Nguyen</strong>, a current college student
+                  who is a tech enthusiast and aspiring full-stack developer 💻
+                </h1>
+              </article>
+            </div>
+          </section>
+          {/**------------------------------------------------- */}
+          <section id="About">
+            <div className="app__about">
+              <div className="app__aboutDescription">
+                <center>
+                  <h1>
+                    {/* <strong> */}
+                    <u>About Me</u>
+                    {/* </strong> */}
+                  </h1>
+                </center>
+                <p>
+                  Hi, I'm Daneil Nguyen, a current college student who is a tech
+                  enthusiast and aspiring full-stack developer.
+                </p>
+                <br />
+                <p>
+                  I am based in the DMV and am attending the University of
+                  Maryland, College Park as an upcoming freshman this fall. I
+                  will be studying Computer Science with a focus on Data
+                  Science.
+                </p>
+                <br />
+                <p>
+                  My overarching goal is to be able to utilize technology to
+                  bring forth positive change in this world. However, I love
+                  programming and have gained an interest in web development and
+                  UI/UX recently. My goal is to learn full-stack development so
+                  that I can create and deploy modern web applications.
+                </p>
+                <br />
 
-      <div className="app__footer">
-        <footer>
-          <h3>©Daneil Nguyen, 2020</h3>
-        </footer>
+                <center>
+                  <h3>Skill Set/Technologies:</h3>
+                </center>
+                <div className="app__skills">
+                  {skillset.map((skill) => (
+                    <div className="app__skillsIndividual">
+                      <h4>⚪{skill.skill}</h4>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="app__profilePic">
+                <img
+                  src={PortfolioPic}
+                  alt="profile pic"
+                  height="400px"
+                  width="350px"
+                />
+              </div>
+            </div>
+          </section>
+          {/**------------------------------------------------- */}
+
+          {/**------------------------------------------------- */}
+          {/**Possibly use firebase so that new projects can be easily added. Map */}
+          <section id="Projects">
+            <div className="app__projects">
+              <center>
+                <h1>My Projects</h1>
+              </center>
+              <div className="app__projectList">
+                {projects.map(({ project }) => (
+                  <ProjectCard
+                    projectName={project.projectName}
+                    image={project.image}
+                    description={project.description}
+                    github={project.github}
+                    technologies={project.technologies}
+                  />
+                ))}
+              </div>
+            </div>
+          </section>
+          {/**------------------------------------------------- */}
+          <section id="Contact">
+            <div className="app__contact">
+              <div className="app__contactText">
+                <center>
+                  <h1>Get In Touch</h1>
+                </center>
+                <p>
+                  My inbox is always open for new opportunities for employment,
+                  internships, etc. Otherwise, you can ask me questions and
+                  simply say hi!
+                </p>
+
+                {/**https://stackoverflow.com/questions/50350085/how-to-make-a-hyperlink-external-in-react */}
+                <div className="app__contactButtons">
+                  <button>
+                    <a
+                      href="mailto: danthedevnguyen@gmail.com"
+                      className="app__contactIcons"
+                    >
+                      <EmailIcon />
+                    </a>
+                  </button>
+                  <button>
+                    <a
+                      href="https://www.linkedin.com/in/daneil-nguyen"
+                      className="app__contactIcons"
+                    >
+                      <LinkedInIcon />
+                    </a>
+                  </button>
+                  <button>
+                    <a
+                      href="https://github.com/codingdn"
+                      className="app__contactIcons"
+                    >
+                      <GitHubIcon />
+                    </a>
+                  </button>
+                  <button>
+                    <a
+                      href="https://www.instagram.com/coding.dan/"
+                      className="app__contactIcons"
+                    >
+                      <InstagramIcon />
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <div className="app__footer">
+            <footer>
+              <h3>©Daneil Nguyen, 2020</h3>
+            </footer>
+          </div>
       </div>
-    </div>
   );
 }
 
